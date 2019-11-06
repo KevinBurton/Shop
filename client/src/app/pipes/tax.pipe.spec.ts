79958a88f6a18f2e54819d1f56da359e58b390d0
Kevin Burton <rkevinburton@charter.net>
2019-11-05 20:44:23 -0600

    Extended tests

--- client/src/app/pipes/tax.pipe.spec.ts
+++ client/src/app/pipes/tax.pipe.spec.ts
@@ -5,4 +5,24 @@ describe('TaxPipe', () => {
     const pipe = new TaxPipe();
     expect(pipe).toBeTruthy();
   });
+  it('calculate tax', () => {
+    const pipe = new TaxPipe();
+    const tax = pipe.transform(1000, 0.10, 0.10);
+    expect(tax).toEqual(200);
+  });
+  it('no tax', () => {
+    const pipe = new TaxPipe();
+    const tax = pipe.transform(1000, 0.0, 0.10);
+    expect(tax).toEqual(100);
+  });
+  it('no duty', () => {
+    const pipe = new TaxPipe();
+    const tax = pipe.transform(1000, 0.10, 0.0);
+    expect(tax).toEqual(100);
+  });
+  it('no tax or duty', () => {
+    const pipe = new TaxPipe();
+    const tax = pipe.transform(1000, 0.0, 0.0);
+    expect(tax).toEqual(0);
+  });
 });
